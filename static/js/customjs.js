852198319f475ca710b92c176511944877ed1c1b
Able c j <gracyebin7@gmail.com>
2025-01-29 17:46:15 +0530

    basic landing page frontend created

--- static/js/customjs.js
+++ static/js/customjs.js
@@ -9,4 +9,14 @@ $(document).ready(function () {
             console.error('Error loading navbar:', error); // Handle any errors
         }
     });
+    $.ajax({
+        url: '../../templates/footer.html', // Load navbar.html file
+        method: 'GET',
+        success: function (data) {
+            $('#footer').html(data); // Insert navbar content into the placeholder
+        },
+        error: function (xhr, status, error) {
+            console.error('Error loading navbar:', error); // Handle any errors
+        }
+    });
 });
